beceb1347526cc9bfb338984ed9244c0d5779101
alderjunio <alder.junio@gmail.com>
2025-01-20 12:55:01 -0400

    Project created

---
+++ src/test/java/br/com/alderjunio/dslist/DslistApplicationTests.java
@@ -0,0 +1,13 @@
+package br.com.alderjunio.dslist;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class DslistApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
